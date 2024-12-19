1930994a144b7710e685c2d1cad5cfe7f4ef4c12
BoskoZlatanovicRAF <bzlatanovic3621rn@raf.rs>
2024-12-19 13:44:42 +0100

    initial commit

---
+++ src/main/kotlin/raf/rs/nwp_project_kotlin/NwpProjectKotlinApplication.kt
@@ -0,0 +1,11 @@
+package raf.rs.nwp_project_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NwpProjectKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<NwpProjectKotlinApplication>(*args)
+}
---
+++ src/main/kotlin/raf/rs/nwp_project_kotlin/enums/PermissionType.kt
@@ -0,0 +1,8 @@
+package raf.rs.nwp_project_kotlin.enums
+
+enum class PermissionType {
+    CAN_READ_USERS,
+    CAN_CREATE_USERS,
+    CAN_UPDATE_USERS,
+    CAN_DELETE_USERS,
+}
